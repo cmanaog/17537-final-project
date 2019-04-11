166be76b7b6c0739d197bd318ff0728ded07f432
KASHISH GARG <kgarg@andrew.cmu.edu>
2019-04-11 14:21:54 -0400

    initial buggy nn from scratch

--- model/AdvNN.py
+++ model/AdvNN.py
@@ -1,17 +1,18 @@
 import sys
-import numpy as np 
+import numpy as np
 import random
 import time
+import pandas as pd
 
 class Ann(object):
-    
+
     def __init__(self, n_in, n_out, n_hidden, learning_rate):
         self.n_in = n_in
         self.n_out = n_out
         self.n_hidden = n_hidden
         self.learning_rate = learning_rate
         self.weights = np.random.uniform(-0.05, 0.05, (n_in, n_hidden))
-        self.out = np.random.uniform(-0.05, 0.05, n_out) # output weights
+        self.out = np.random.uniform(-0.05, 0.05, n_out) # output weights: 1 x n_hidden
         self.hidden_layer_out = np.zeros(n_out)
         self.first_layer_deriv = np.zeros(n_out)
         self.output_deriv = 0
@@ -28,18 +29,29 @@ class Ann(object):
             [4] = F[0, 100]
         @return: result (int) - 0 or 1, classification of node
         '''
-        data = np.matrix(train_vec)
+        data = np.matrix(train_vec) # 1 x n_in
         # print(np.append(np.array(data.dot(self.weights)), 1))
-        first_layer = sigmoid(data.dot(self.weights)) # 1 x 3 (# hidden units)
-        first_layer = np.append(np.array(first_layer), 1) # bias term, 1 x 4
-        first_layer_deriv = np.append(np.array(data.dot(self.weights)), 1)
-        result = sigmoid(first_layer.dot(self.out)) # should be constant (1 x 1)
+        # for each elem in data, make it 0, if it's neg
+        #
+        first_layer = self.relu(data.dot(self.weights)) # 1 x n_hidden
+        first_layer = np.append(np.array(first_layer), 1) # adding bias term, 1 x n_hidden + 1
+
+        # the vector that feeds into the activation function
+        first_layer_into_node = np.append(np.array(data.dot(self.weights)), 1) # 1 x n_hidden + 1
+
+        # TODO: logit = apply sigmoid to each elem of first_layer
+        logit = first_layer
+
+        result = self.sigmoid(first_layer.dot(self.out)) # probablity of 0 or 1
+        if result > 0.5: result = 1
+        else: result = 0
+
         if store: # save computation if we're not using backprop
             self.hidden_layer_out = first_layer
-            self.output_deriv = sigmoid(first_layer.dot(self.out), True)
-            self.first_layer_deriv = sigmoid(first_layer_deriv, True)
-            self.first_layer_deriv[-1] = 1
-        return result * 100
+            self.output_deriv = self.sigmoid(first_layer.dot(self.out), True)
+            self.first_layer_deriv = map(self.relu_deriv, first_layer_into_node)
+            self.first_layer_deriv[-1] = 1 # We think this is bias
+        return (logit, result)
 
     def update_weights(self, output, truth, train):
         '''
@@ -48,41 +60,42 @@ class Ann(object):
         @param: train(1d list) - input values
         @return: None - updates weights of neural net using grad. desc.
         '''
-        error = truth - output # constant
+        error = truth - output # constant La
         self.delta_k = error * self.output_deriv
-        self.delta_h = self.first_layer_deriv * self.out * self.delta_k 
+        self.delta_h = self.first_layer_deriv * self.out * self.delta_k
+
+        # update the weights b/w hidden layer and output
         gradient_top = self.learning_rate * self.delta_k * self.hidden_layer_out
         self.out = self.out + gradient_top
+
+
+        # update the weights b/w hidden layer and input
         self.delta_h = np.delete(self.delta_h, -1) # remove bias
-        gradient_bottom = self.learning_rate * np.matrix(train).transpose() * np.matrix(self.delta_h)  
+        gradient_bottom = self.learning_rate * np.matrix(train).transpose() * np.matrix(self.delta_h)
         self.weights = self.weights + gradient_bottom
 
-def norm(arr):
-    '''
-    @param: arr (1d array) - array to normalize
-    @return: new_arr (1d array) - normalized array
-    '''
-    return arr / 100
 
-def parse(data):
-    '''
-    @param: data (2d list) - unnormalized data 
-    @return: new (2d list) - nornalized data and biased added
-    '''
-    new = data
-    return np.apply_along_axis(norm, 1, data)
+    def softmax(x):
+        return np.exp(x)/sum(np.exp(x))
 
-def sigmoid(net, deriv = False):
-    '''
-    @param: net (float) - a weighted sum
-    @param: deriv (bool) - indicates whether to use derivative or not
-    '''
-    if deriv:
-        return sigmoid(net) * (1 - sigmoid(net))
-    else:
-        return 1.0 / (1.0 + np.exp(-1.0 * net))
+    def relu(x):
+        return np.maximum(np.zeros(self.n_hidden), x)
 
-def backpropogate(train, target, n_in, n_out, n_hidden, learning_rate = 0.1):
+    def relu_deriv(x):
+        if (x > 0): return 1
+        else: return 0
+
+    def sigmoid(net, deriv = False):
+        '''
+        @param: net (float) - a weighted sum
+        @param: deriv (bool) - indicates whether to use derivative or not
+        '''
+        if deriv:
+            return sigmoid(net) * (1 - sigmoid(net))
+        else:
+            return 1.0 / (1.0 + np.exp(-1.0 * net))
+
+def train(train, target, n_in, n_out, n_hidden, learning_rate = 0.1):
     '''
     @param: train (2d list) - training input data
     @param: target (1d list) - output data from training data
@@ -102,43 +115,51 @@ def backpropogate(train, target, n_in, n_out, n_hidden, learning_rate = 0.1):
         # For each training vector and output,
         for row in range(train.shape[0]):
             # Input x to the network and computer output o_u
-            output = network.feed_forward(train[row], store = True)
-            # update weights
-            network.update_weights(output, target[row], train[row])
+            (logit, output) = network.feed_forward(train[row], store = True)
+
+            # update weights of approval network (Ly)
+            la = network.update_weights(output, target[row], train[row])
+
+            # get output for race
+                # another feed_forward with logit as the input
+
+            # update the weight for race nn (Ld)
+            # ld = network.update_weights(output_race, race_col_from_train_x, logit)
+
+            # L = ly - alpha * Ld
+
+
             error_rate += (target[row] - output)**2
         count += 1
-        sys.stdout.write(str(0.5 * error_rate) + "\n")
+        print("error rate = " + str(0.5 * error_rate))
     return network
 
-def main(train, target, test):
+def main():
     '''
     @param: train (str) - path to training data (2d)
     @param: target (str) - path to target data (1d)
     @param: test (str) - path to testing data (2d)
     '''
     ################## TUNING ########################
-    n_hidden = 4 # magic number, change to tune neural net
+    n_hidden = 256 # magic number, change to tune neural net
     eta = 0.01 # magic number, change to tune neural net
     ##################################################
-    train_data = np.genfromtxt(train, delimiter = ",", dtype = float, skip_header = 1)
-    train_data = parse(train_data)
-    keys = np.genfromtxt(target, dtype = float) # target data
-    test_data = np.genfromtxt(test, delimiter = ",", dtype = float, skip_header = 1)
-    test_data = parse(test_data)
-    n_in = train_data.shape[1]
-    n_out = n_hidden + 1# only one output
+    train_data = pd.read_csv("~/Downloads/train_clean.csv")
+    x_train = np.asarray(train_data.iloc[:, 1:-1], dtype = np.float64)
+    y_train = np.asarray(train_data.iloc[:, -1], dtype = np.float64)
+
+    test_data = pd.read_csv("~/Downloads/test_clean.csv")
+    x_test = np.asarray(test_data.iloc[:, 1:-1], dtype = np.float64)
+    y_test = np.asarray(test_data.iloc[:, -1], dtype = np.float64)
+
+    n_in = x_train.shape[1]
+    n_out = n_hidden + 1 # only one output
     start = time.time()
-    trained_nn = backpropogate(train_data, keys, n_in, n_out, n_hidden, eta)
+    trained_nn = train(x_train, y_train, n_in, n_out, n_hidden, eta)
     end = time.time()
     sys.stdout.write("TRAINING COMPLETED! NOW PREDICTING.\n")
     for row in test_data:
         output = trained_nn.feed_forward(row)
         sys.stdout.write(str(np.round(output)) + "\n")
 
-if __name__ == "__main__":
-    # file1, file2, file3 = (sys.argv[1], sys.argv[2], sys.argv[3])
-    file1, file2, file3 = ("education_train.csv",
-                          "education_train_keys.txt",
-                          "education_dev.csv")
-    main(file1, file2, file3)
-
+main()
